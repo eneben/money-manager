71d29d33c2488c587fe0a43026727ea5404dd893
Elisabeth Neben <elisabeth.neben@posteo.de>
2025-06-10 19:27:25 +0200

    add basic link to form

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,17 @@ export default function HomePage() {
           return <Transaction key={transaction.id} transaction={transaction} />;
         })}
       </StyledTransactionList>
+      <StyledLink>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="36px"
+          viewBox="0 -960 960 960"
+          width="36px"
+          fill="var(--background-color)"
+        >
+          <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+        </svg>
+      </StyledLink>
     </>
   );
 }
@@ -21,4 +32,19 @@ const StyledHeadline = styled.h2`
 `;
 const StyledTransactionList = styled.ul`
   list-style: none;
+`;
+
+const StyledLink = styled.a`
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  width: 60px;
+  height: 60px;
+  background-color: var(--primary-light);
+  color: var(--background-color);
+  border: 1px solid var(--background-color);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
